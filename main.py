12e3531e93dfed15a923151069d0d662fab64c8b
tefy <tefy.rakotomahefa@gmail.com>
2020-07-25 14:56:30 +0300

    now use variables environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import selenium.webdriver as webdriver
-import time
 import configparser
+import os
+import selenium.webdriver as webdriver
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -10,10 +10,10 @@ driver = webdriver.Chrome()
 driver.get("https://moncompte.orange.mg/")
 
 phone_number = driver.find_element_by_name("phone-numb")
-phone_number.send_keys(config["default"]["phone_number"])
+phone_number.send_keys(os.environ.get('PHONE_NUMBER'))
 
 password = driver.find_element_by_name("password")
-password.send_keys(config["default"]["password"])
+password.send_keys(os.environ.get('PASSWORD'))
 
 login = driver.find_element_by_id("login_button")
 login.click()
@@ -26,3 +26,8 @@ data = parent.find_element_by_class_name("kpi-value")
 
 print(f"{date.text}: {data.text}")
 
+report = open("internet_report.txt", "a")
+report.write(f"\n{date.text}: {data.text}")
+report.close()
+
+driver.quit()
